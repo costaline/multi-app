4db09df388f5e901b457862288e168ba2b8e29bf
Constantine Zubarev <kv.zubarev@gmail.com>
2022-11-04 12:12:15 +0600

    lint: stylelint
    * css
    * html
    * vue
    * astro

---
+++ packages/stylelint-config-custom/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+	defaultSeverity: 'warning',
+
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-css-modules',
+		"stylelint-config-html",
+		'stylelint-config-clean-order',
+		// 'stylelint-config-hudochenkov/order',
+		'stylelint-config-prettier',
+	],
+
+	rules: {
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				except: ['blockless-after-same-name-blockless', 'first-nested'],
+				ignoreAtRules: ['else', 'include'],
+			},
+		],
+		'at-rule-no-unknown': null,
+		'color-hex-case': 'lower',
+		'color-hex-length': 'long',
+		'declaration-block-trailing-semicolon': 'always',
+		'declaration-colon-space-after': 'always-single-line',
+		'declaration-colon-space-before': 'never',
+		'indentation': 'tab',
+		'rule-empty-line-before': [
+			'always',
+			{
+				except: ['after-single-line-comment', 'first-nested'],
+			},
+		],
+		'selector-class-pattern': null,
+		'max-empty-lines': [1, {ignore: ['comments']}]
+	},
+}
