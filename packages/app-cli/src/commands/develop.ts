8fb4e997d47d00e5a7d88669a39c8b751e00b574
Constantine Zubarev <kv.zubarev@gmail.com>
2022-12-23 21:13:37 +0600

    feat(app-cli): interactive

--- packages/app-cli/src/commands/develop.ts
+++ packages/app-cli/src/commands/develop.ts
@@ -1,5 +1,7 @@
 import { Command } from 'commander'
+import inquirer from 'inquirer'
 
+import { RUNNABLE_APPS } from '../constants'
 import { cmdSpawn } from '../helpers/cmd'
 
 export function makeDevCommand(): Command {
@@ -9,6 +11,7 @@ export function makeDevCommand(): Command {
 		.description('development mode')
 		.option('-d, --docker', 'run with docker')
 		.option('-f, --filter <apps...>', 'specify apps')
+		.option('-i, --interactive', 'specify apps interactively')
 		.action(action)
 
 	return dev
@@ -17,6 +20,7 @@ export function makeDevCommand(): Command {
 interface Options {
 	docker?: boolean
 	filter?: string[]
+	interactive?: boolean
 }
 
 function action(options: Options): void {
@@ -31,6 +35,38 @@ function action(options: Options): void {
 			'up',
 			'--build',
 		])
+	} else if (options.interactive) {
+		inquirer
+			.prompt([
+				{
+					type: 'checkbox',
+					message: 'Select apps',
+					name: 'apps',
+					choices: [
+						...Object.entries(RUNNABLE_APPS).reduce<
+							Array<inquirer.Separator | { name: string }>
+						>((acc, [type, apps]) => {
+							return [
+								...acc,
+								new inquirer.Separator(` = ${type} apps = `),
+								...apps.map((app) => ({ name: app })),
+							]
+						}, []),
+					],
+				},
+			])
+			.then((answers) => {
+				const apps = answers['apps'].map((app: string) => `--filter=${app}`)
+
+				cmdSpawn('yarn', ['dev', ...apps])
+			})
+			.catch((error) => {
+				if (error.isTtyError) {
+					console.log("Prompt couldn't be rendered in the current environment")
+				} else {
+					console.log('Something else went wrong')
+				}
+			})
 	} else {
 		if (options.filter?.length) {
 			const apps = options.filter.map((app) => `--filter=${app}`)
--- packages/app-cli/src/constants.ts
+++ packages/app-cli/src/constants.ts
@@ -1,3 +1,9 @@
 export const ENVS_BY_APP: Record<string, string[]> = {
 	strapi: ['.env', '.env.development'],
 }
+
+export const RUNNABLE_APPS = {
+	side: ['docs'],
+	main: ['about', 'home', 'strapi'],
+	ancillary: ['lit-wc'],
+} as const
