365edd49ae3a118331f6c2c8b6b211cca96f28eb
Constantine Zubarev <kv.zubarev@gmail.com>
2022-12-26 13:03:25 +0600

    style(docs): add tailwind

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [tailwind()],
 	server: {
 		port: Number(process.env.PORT || process.env.APP_DOCS_PORT) || 2000
 	}
