37ba440e2174cdcc65096d0a4ef227230f4397bf
Constantine Zubarev <kv.zubarev@gmail.com>
2022-12-30 18:45:47 +0600

    feat(docs): base layout

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), mdx()],
 	server: {
 		port: Number(process.env.PORT || process.env.APP_DOCS_PORT) || 2000
-	}
+	},
 });
