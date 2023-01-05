2538b76ff4652fcb45a461ec946da089d88243f0
Constantine Zubarev <kv.zubarev@gmail.com>
2023-01-05 14:13:48 +0600

    test(home): base

--- apps/home/vite.config.ts
+++ apps/home/vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { loadEnv } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -21,6 +22,11 @@ export default defineConfig(({ mode }) => {
 			watch: {
 				usePolling: true,
 			},
-		}
+		},
+
+		test: {
+			environment: 'happy-dom',
+			setupFiles: './setupTests.ts',
+		},
 	}
 })
