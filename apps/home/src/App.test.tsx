70ee17df062ff902df461cb0e7729c3656e44773
Constantine Zubarev <kv.zubarev@gmail.com>
2023-01-05 14:16:47 +0600

    refactor(home): tests config

--- apps/home/src/App.test.tsx
+++ apps/home/src/App.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
 
 import App from './App'
 
--- apps/home/vite.config.ts
+++ apps/home/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
 		},
 
 		test: {
+			globals: true,
 			environment: 'happy-dom',
 			setupFiles: './setupTests.ts',
 		},
