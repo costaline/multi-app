16337049a28afabcc0455797d0116e65ca07f690
Constantine Zubarev <kv.zubarev@gmail.com>
2023-02-06 14:08:45 +0600

    feat(svelte-wc): storybook

--- packages/svelte-wc/.eslintrc.cjs
+++ packages/svelte-wc/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true,
-	extends: ["custom", "custom/svelte.js"],
+	extends: ["custom", "custom/svelte.js", "custom/storybook.js"],
 	rules: {
 		'svelte/valid-compile': 'off'
 	}
